d373f5cfabb0561519bf9f1e92bf5af9a1a2b436
HZKMaurice <zikaiho@gmail.com>
2023-03-18 22:42:52 +0800

    feat: delete workflow

--- src/views/AllForms.vue
+++ src/views/AllForms.vue
@@ -29,7 +29,7 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="item in data" :key="item.id">
+                    <tr v-for="item, index in data" :key="item.id">
                         <th>
                             <span style="font-weight: 500">{{ item.name }}</span
                             ><br />
@@ -44,7 +44,7 @@
                                 <img src="../assets/images/editIcon.png" height="25" />
                             </button>
                             <button class="rounded bg-white">
-                                <img src="../assets/images/deleteIcon.png" height="25" />
+                                <img src="../assets/images/deleteIcon.png" height="25" @click="deleteWorkflow(item.formUuid, index )" />
                             </button>
                         </td>
                     </tr>
@@ -83,6 +83,55 @@ async function createNewEntry() {
     // newEntryId.value = response.data;
 
     router.push({ name: 'newWorkflow' })
+
+}
+function deleteWorkflow(formUuid, index){
+    axios.get('http://localhost:8080/api/formSteps/byParentForm/'+formUuid)
+    .then(response => {
+        const fullFormdata = response.data
+        console.log(fullFormdata[0])
+        for (let index = 0; index < fullFormdata[0].formSteps.length; index++) {
+            const stepUuid =  fullFormdata[0].formSteps[index].stepUuid;
+
+            const associatedSubform = fullFormdata[0].formSteps[index].associatedSubform;
+            for (let index = 0; index < associatedSubform.length; index++) {
+                const associatedSubformsId = associatedSubform[index].associatedSubformsId;
+                axios.delete('http://localhost:8080/api/associatedSubform/'+associatedSubformsId)
+                // eslint-disable-next-line no-unused-vars
+                .then(response => {
+                    console.log('associatedSubformsId deleted successfully');
+                })
+                .catch(error => {
+                    console.error('Error deleting associatedSubformsId', error);
+                });
+            }
+            axios.delete('http://localhost:8080/api/formSteps/'+stepUuid)
+                // eslint-disable-next-line no-unused-vars
+                .then(response => {
+                    console.log('stepUuid deleted successfully');
+                })
+                .catch(error => {
+                    console.error('Error deleting stepUuid', error);
+                });
+        }
+    })
+    .catch(error => {
+        console.error('errorgetting', error);
+    });
+
+
+
+    axios.delete('http://localhost:8080/api/formWorkflows/'+formUuid)
+    // eslint-disable-next-line no-unused-vars
+    .then(response => {
+        console.log('formUuid deleted successfully');
+        data.value.splice(index,1)
+    
+    })
+    .catch(error => {
+        console.error('Error deleting formUuid', error);
+    });
+
 
 }
 
