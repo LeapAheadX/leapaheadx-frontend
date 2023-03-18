d4c842c303ca441d0435c4c90286e03941c3b8f1
HZKMaurice <zikaiho@gmail.com>
2023-03-19 01:37:56 +0800

    feat: editing form

--- src/views/AllForms.vue
+++ src/views/AllForms.vue
@@ -40,7 +40,7 @@
                         <td class="align-middle">{{ item.dateCreated }}</td>
 
                         <td class="align-middle">
-                            <button @click="editworkflow()" class="rounded bg-white me-4">
+                            <button @click="editWorkflow(item.formUuid)" class="rounded bg-white me-4">
                                 <img src="../assets/images/editIcon.png" height="25" />
                             </button>
                             <button class="rounded bg-white">
@@ -72,67 +72,52 @@ onMounted(async () => {
     data.value = response.data
 })
 
+function editWorkflow(formUuid) {
+  router.push({ name: 'editWorkflow', params: { formUuid: formUuid } })
+}
 
 async function createNewEntry() {
-    // const postdata = {
-    //     "name": "sdfsdfsd",
-    //     "description": "this is the process of getting bto",
-    //     "createdBy": "79ebaf36-bd58-11ed-afa1-0242ac120002"
-    // }
-    // const response = await axios.post('http://localhost:8080/api/formWorkflows', postdata);
-    // newEntryId.value = response.data;
+
 
     router.push({ name: 'newWorkflow' })
 
 }
 function deleteWorkflow(formUuid, index){
+
     axios.get('http://localhost:8080/api/formSteps/byParentForm/'+formUuid)
     .then(response => {
         const fullFormdata = response.data
         console.log(fullFormdata[0])
+        const deletePromises = [];
         for (let index = 0; index < fullFormdata[0].formSteps.length; index++) {
             const stepUuid =  fullFormdata[0].formSteps[index].stepUuid;
 
             const associatedSubform = fullFormdata[0].formSteps[index].associatedSubform;
             for (let index = 0; index < associatedSubform.length; index++) {
                 const associatedSubformsId = associatedSubform[index].associatedSubformsId;
-                axios.delete('http://localhost:8080/api/associatedSubform/'+associatedSubformsId)
-                // eslint-disable-next-line no-unused-vars
-                .then(response => {
-                    console.log('associatedSubformsId deleted successfully');
-                })
-                .catch(error => {
-                    console.error('Error deleting associatedSubformsId', error);
-                });
+                deletePromises.push(axios.delete('http://localhost:8080/api/associatedSubform/'+associatedSubformsId));
             }
-            axios.delete('http://localhost:8080/api/formSteps/'+stepUuid)
-                // eslint-disable-next-line no-unused-vars
-                .then(response => {
-                    console.log('stepUuid deleted successfully');
-                })
-                .catch(error => {
-                    console.error('Error deleting stepUuid', error);
-                });
+            deletePromises.push(axios.delete('http://localhost:8080/api/formSteps/'+stepUuid));
         }
+        Promise.all(deletePromises)
+            .then(() => {
+                axios.delete('http://localhost:8080/api/formWorkflows/'+formUuid)
+                    // eslint-disable-next-line no-unused-vars
+                    .then(response => {
+                        console.log('formUuid deleted successfully');
+                        data.value.splice(index,1)
+                    })
+                    .catch(error => {
+                        console.error('Error deleting formUuid', error);
+                    });
+            })
+            .catch(error => {
+                console.error('Error deleting associatedSubformsId or stepUuid', error);
+            });
     })
     .catch(error => {
         console.error('errorgetting', error);
     });
-
-
-
-    axios.delete('http://localhost:8080/api/formWorkflows/'+formUuid)
-    // eslint-disable-next-line no-unused-vars
-    .then(response => {
-        console.log('formUuid deleted successfully');
-        data.value.splice(index,1)
-    
-    })
-    .catch(error => {
-        console.error('Error deleting formUuid', error);
-    });
-
-
 }
 
 
--- src/views/NewWorkflow.vue
+++ src/views/NewWorkflow.vue
@@ -13,7 +13,6 @@
           
                         <button type="button" class="btn btn-light" @click="addStepcomponent()">Add Step</button>
                     </div>
-                    
                     <div v-for="(step, index) in steps" :key="index" class="my-3">
                         <div class="d-flex justify-content-between">   
                             Step {{ index + 1}}
@@ -51,7 +50,6 @@
             
             <div class="col-1" >
             </div>
-
             <div class="rounded-4 p-5 mt-4 col-5" style="background-color: #0f1726;">
                 <div class="row">
                     <h6 class="col">All Form components</h6>
@@ -59,6 +57,7 @@
                 </div>
                 <div  v-for="item in subformcanvasData" :key="item.canvasUuid" class="row mt-3 text-center" draggable="true" @dragstart="onDragStart(item.canvasUuid)">
                     <div class="col rounded py-3" style="background-color: #707279;">
+                        
                         {{ item.name }} <br>
                         <button type="button" class="btn btn-light col-2 mt-2" @click="addtostep(item.canvasUuid)"> Drag </button>
                     </div>
@@ -71,12 +70,54 @@
 <script setup>
 import axios from 'axios'
 import { ref, onMounted } from 'vue'
-// import { useRoute } from 'vue-router';
+import { useRoute , useRouter } from 'vue-router';
 const subformcanvasData = ref([])
 const workflowname = ref("")
-// const route = useRoute();
-// const formUuid = route.params.formUuid
+const route = useRoute();
+const formUuid = route.params.formUuid
 const steps = ref([]);
+const router = useRouter()
+
+async function loadFormData(formUuid) {
+  try {
+    const response = await axios.get('http://localhost:8080/api/formSteps/byParentForm/'+formUuid);
+    const oldFormData = response.data;
+    if (!oldFormData || !oldFormData[0]) {
+      console.error('Error: no data found');
+      return;
+    }
+    
+    workflowname.value = oldFormData[0].workflowName
+        for (const step of oldFormData[0].formSteps) {
+            const droppedItems = ref([]) // later add to steps.value
+            // console.log(step.associatedSubform)
+            for (const associatedSubform of step.associatedSubform) {
+                // (associatedSubform.name)
+                const subformcanvasDatatemp = subformcanvasData.value;
+                for (const subCanData of subformcanvasDatatemp) {
+                    if (subCanData.canvasUuid ==associatedSubform.name){
+                        droppedItems.value.push(subCanData)
+                    }
+                }
+            }
+            steps.value.push({
+                assigneeType: step.assigneeType, 
+                orderNo: step.orderNo, 
+                action: step.action,
+                droppedItems:droppedItems, 
+            })
+        }
+  } catch (error) {
+    console.error('Error loading form data', error);
+  }
+}
+
+if (formUuid) {
+  loadFormData(formUuid);
+}
+
+
+// end of editing
 
 function onDragStart(canvasUuid) {
   event.dataTransfer.setData('text/plain', canvasUuid);
@@ -137,8 +178,6 @@ function addStepcomponent() {
     steps.value.push(newStep);
 }
 
-
-
 function save() {
   let isValid = true;
   var message = '';
@@ -163,12 +202,14 @@ function save() {
     });
 
 
-
   }
   if (isValid) {
         // Perform save operation
-        saving()
-       
+        if (formUuid == null){
+            saving()
+        }else{
+            deleting()
+        }
     } else {
         // Show validation error message
         alert(message);
@@ -176,11 +217,45 @@ function save() {
 
 
 }
+const deleting = async () => {
+    axios.get('http://localhost:8080/api/formSteps/byParentForm/'+formUuid)
+    .then(response => {
+        const fullFormdata = response.data
+        console.log(fullFormdata[0])
+        const deletePromises = [];
+        for (let index = 0; index < fullFormdata[0].formSteps.length; index++) {
+            const stepUuid =  fullFormdata[0].formSteps[index].stepUuid;
+
+            const associatedSubform = fullFormdata[0].formSteps[index].associatedSubform;
+            for (let index = 0; index < associatedSubform.length; index++) {
+                const associatedSubformsId = associatedSubform[index].associatedSubformsId;
+                deletePromises.push(axios.delete('http://localhost:8080/api/associatedSubform/'+associatedSubformsId));
+            }
+            deletePromises.push(axios.delete('http://localhost:8080/api/formSteps/'+stepUuid));
+        }
+        Promise.all(deletePromises)
+            .then(() => {
+                axios.delete('http://localhost:8080/api/formWorkflows/'+formUuid)
+                    // eslint-disable-next-line no-unused-vars
+                    .then(response => {
+                        console.log('formUuid deleted successfully');
+                        saving();
+                    })
+                    .catch(error => {
+                        console.error('Error deleting formUuid', error);
+                    });
+            })
+            .catch(error => {
+                console.error('Error deleting associatedSubformsId or stepUuid', error);
+            });
+    })
+    .catch(error => {
+        console.error('errorgetting', error);
+    });
+}
+
 
 const saving = async () => {
-   
-
-
     const workflowdata = {
         "name": workflowname.value,
         "description": "this is the process of getting bto",
@@ -228,20 +303,19 @@ const saving = async () => {
             console.log(error)
             console.log("2 formstep failed")
         }
-        alert("Sucessfully added workflow");
             
     }
-
+    alert("Sucessfully added workflow");
+    router.push({ name: 'allforms' })
     }catch (error){
         alert("Workflow name is taken")
 
     }
-
 }
-
 onMounted(async () => {
     const response = await axios.get('http://localhost:8080/api/subformcanvas')
     subformcanvasData.value = response.data
+
 })
 </script>
 
